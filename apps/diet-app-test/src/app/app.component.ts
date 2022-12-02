fc6db23e4423a62ebdaaeed9073add054d71189c
cxdima <cxdimafeed@gmail.com>
2022-12-02 01:20:13 -0600

    Added components (not finished)

--- apps/diet-app-test/src/app/app.component.ts
+++ apps/diet-app-test/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './recipe/recipe';
 
 @Component({
   selector: 'diet-app-test-root',
@@ -7,4 +8,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'diet-app-test';
+  products = productsList;
 }
+
+export const productsList: Array<Recipe>= [
+  {
+    id: 1,
+    rating: 1,
+    stars: 5,
+    image: '/assets/Example_7.webp',
+    name: 'Spaghetti Carbonara',
+    time: 10,
+    difficulty: 'easy',
+
+  },
+  {
+    id: 2,
+    rating: 3,
+    stars: 5,
+    image: '/assets/Example_6.webp',
+    name: 'Steak',
+    time: 30,
+    difficulty: 'medium'
+  },
+  {
+    id: 3,
+    rating: 5,
+    stars: 5,
+    image: '/assets/Example_8.webp',
+    name: 'Muffins',
+    time: 20,
+    difficulty: 'hard'
+  },
+  {
+      id: 3,
+      rating: 5,
+      stars: 5,
+      image: '/assets/Example_8.webp',
+      name: 'Muffins',
+      time: 20,
+      difficulty: 'hard'
+    },
+    {
+        id: 3,
+        rating: 5,
+        stars: 5,
+        image: '/assets/Example_8.webp',
+        name: 'Muffins',
+        time: 20,
+        difficulty: 'hard'
+      },
+      {
+          id: 3,
+          rating: 5,
+          stars: 5,
+          image: '/assets/Example_8.webp',
+          name: 'Muffins',
+          time: 20,
+          difficulty: 'hard'
+        },
+];
--- apps/diet-app-test/src/app/app.module.ts
+++ apps/diet-app-test/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { StarComponent } from './star/star.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,RecipeComponent, StarComponent],
   imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
